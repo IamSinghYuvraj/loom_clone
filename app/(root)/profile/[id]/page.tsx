21e166b3cc8b32d1bf633a154b60ac5770bfb5c7
IamSinghYuvraj <121106713+IamSinghYuvraj@users.noreply.github.com>
2025-06-18 19:52:46 +0530

    .

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import { dummyCards } from "@/constants";
+import VideoCard from "@/components/VideoCard";
+
+const Page = async ({ params }: ParamsWithSearch) => {
+  const { id } = await params;
+
+  return (
+    <div className="wrapper page">
+      <Header
+        subHeader="YuvrajSinghispro"
+        title="Yuvraj | UV"
+        userImg="/assets/images/dummy.jpg"
+      />
+
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Page;
